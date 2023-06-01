1bda19d455d1091b99bb5bd4b6564473cdfa72ed
Starlight <StarlightWT@protonmail.com>
2023-06-01 09:48:48 +0200

    updated reporting per 50 images

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const { token } = require("./src/src.json");
 const XMLHttpRequest = require("xhr2");
 const { EmbedBuilder } = require("@discordjs/builders");
 let request = new XMLHttpRequest();
-
+const channels = [
+  "1113725807328755782",
+  "1113725846721679390",
+  "1113732533176258641",
+];
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -12,47 +16,57 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
   var embed;
+  var colour;
   var subreddit;
   var imageLink = "";
+  var j = 0;
   var subredditCount = 0;
+  var totalImages = 0;
   setInterval(async () => {
-    let channel = await client.channels.cache.get("1113409427224145923");
-
     switch (subredditCount) {
       case 0:
         subreddit = "hentaibondage";
+        colour = 0xd400ff;
         subredditCount++;
         break;
       case 1:
         subreddit = "yurihentai";
+        colour = 0xff00b2;
         subredditCount++;
         break;
       case 2:
         subreddit = "hentaipetgirls";
+        colour = 0xff004a;
         subredditCount++;
         break;
       case 3:
         subreddit = "yuri";
+        colour = 0xff0e00;
         subredditCount++;
         break;
       case 4:
         subreddit = "Hentai_Bondage";
+        colour = 0xff7000;
         subredditCount++;
         break;
       case 5:
         subreddit = "GenshinImpactHentai";
+        colour = 0xffcd00;
         subredditCount++;
         break;
       case 6:
         subreddit = "GenshinImpactNSFW";
+        colour = 0x57ff00;
         subredditCount++;
         break;
       case 7:
         subreddit = "GenshinBondage";
+        colour = 0x00ff21;
         subredditCount++;
         break;
       case 8:
         subreddit = "GenshinYuri";
+        colour = 0x02fd9c;
         subredditCount++;
         break;
       default:
@@ -97,16 +111,28 @@ client.once(Events.ClientReady, (c) => {
           );
         }
         i++;
+        totalImages++;
       });
       i = 0;
-      embed = new EmbedBuilder()
-        .setTitle("A wild kinky image appears:")
-        .setImage(`${imageLink}`)
-        .setTimestamp();
-      channel.send({
-        embeds: [embed],
+
+      channels.forEach(async (channelId) => {
+        embed = new EmbedBuilder()
+          .setTitle("A wild kinky image appears:")
+          .setImage(`${imageLink}`)
+          .setTimestamp()
+          .setColor(colour);
+
+        let channel = await client.channels.fetch(channelId);
+        channel.send({
+          embeds: [embed],
+        });
       });
     };
+    j++;
+    if (j == 50) {
+      console.log(`Total images posted: ${totalImages}`);
+      j = 0;
+    }
   }, 10000);
 });
 
